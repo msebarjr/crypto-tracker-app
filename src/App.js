d9e01972522d3b856070dec3b025dec1ade2368f
Michael Sebar Jr <msebarjr@gmail.com>
2022-06-28 00:07:52 -0400

    signup form complete with responsiveness

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
+import Signup from "./pages/Signup";
 
 function App() {
     return (
         <div className="app_container">
             <Navbar />
-            <Login />
+            {/* <Login /> */}
+            <Signup />
         </div>
     );
 }
