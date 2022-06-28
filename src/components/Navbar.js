dd8901c619e6e4ac97e4449cb54544c22fe40283
Michael Sebar Jr <msebarjr@gmail.com>
2022-06-27 22:50:06 -0400

    Updated nav with resposiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,25 +5,23 @@ import Button from "./UI/Button";
 
 function Navbar() {
     return (
-        <header>
-            <nav className={styles.nav}>
-                <div className={styles.logo}>
-                    <RiCoinsLine className={styles.icon} />
-                    <p>
-                        <span>Crypto</span>
-                        Tracker
-                    </p>
-                    <RiCoinsLine className={styles.icon} />
-                </div>
-                <div className={styles.links}>
-                    <Button style={styles.link}>Coins</Button>
-                    <Button style={styles.link}>Portfolio</Button>
-                </div>
-                <div className={styles.logout}>
-                    <Button style={styles.logout_button}>Logout</Button>
-                </div>
-            </nav>
-        </header>
+        <nav className={styles.nav}>
+            <div className={styles.logo}>
+                <RiCoinsLine className={styles.icon} />
+                <p>
+                    <span>Crypto</span>
+                    Tracker
+                </p>
+                <RiCoinsLine className={styles.icon} />
+            </div>
+            <div className={styles.links}>
+                <Button style={styles.link}>Coins</Button>
+                <Button style={styles.link}>Portfolio</Button>
+            </div>
+            <div className={styles.logout}>
+                <Button style={styles.logout_button}>Logout</Button>
+            </div>
+        </nav>
     );
 }
 
