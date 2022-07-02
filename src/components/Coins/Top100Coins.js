5f7752eb965bf457b8623c632dc5099c708ef53f
Michael Sebar Jr <msebarjr@gmail.com>
2022-07-02 13:49:06 -0400

    created table files

--- src/components/Coins/Top100Coins.js
+++ src/components/Coins/Top100Coins.js
@@ -2,8 +2,19 @@ import Card from "../UI/Card";
 
 import styles from "../../styles/Top100Coins.module.css";
 
-function Top100Coins() {
-    return <Card style={styles.top_100_card}>Coins </Card>;
+function Top100Coins({ coins }) {
+    return <Card style={styles.top_100_card}> </Card>;
 }
 
 export default Top100Coins;
+
+/**
+ * star
+ * #
+ * image - name
+ * symbol
+ * 24hr
+ * 24hr volume
+ * mkt
+ * last 7 days sparkline
+ */
---
+++ src/components/Table/Table.js
@@ -0,0 +1,11 @@
+import styles from "../../styles/Table.module.css";
+
+function Table({ coins }) {
+    return (
+        <div>
+            <table></table>
+        </div>
+    );
+}
+
+export default Table;
