e11193d4ec5c3f8639640899e60ddda986572658
Michael Sebar Jr <msebarjr@gmail.com>
2022-07-04 02:14:04 -0400

    Added Links and dynamic data to coin-page

--- src/components/Coins/CoinPage.js
+++ src/components/Coins/CoinPage.js
@@ -1,7 +1,22 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 function CoinPage() {
-    return <div>CoinPage</div>;
+    const location = useLocation();
+    const coinId = location.state;
+
+    const [coin, setCoin] = useState({});
+    const url = `https://api.coingecko.com/api/v3/coins/${coinId}?localization=false&sparkline=true`;
+
+    useEffect(() => {
+        axios.get(url).then((response) => {
+            setCoin(response.data);
+            console.log(response.data);
+        });
+    }, [url]);
+
+    return <div>Hello</div>;
 }
 
 export default CoinPage;
